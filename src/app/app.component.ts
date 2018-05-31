4151e3bda46583e7b9af0c7b197f817a66d59a95
Vinicius Cechin <vinicechin@gmail.com>
2018-05-31 19:38:50 -0300

    Filter even numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
   onlyOdd = false;
+
+  getNumbers() {
+    if (this.onlyOdd) {
+      return this.numbers.reduce(function(arr, num) { 
+        if (num % 2 !== 0) arr.push(num); 
+        return arr; 
+      }, []);
+    } else {
+      return this.numbers;
+    }
+  }
 }

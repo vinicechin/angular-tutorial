2eaee69ac7002f9aa6283c087cb68546eb669854
Vinicius Cechin <vinicechin@gmail.com>
2018-06-12 12:26:24 -0300

    Finished reducer

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -1,8 +1,6 @@
-import { Action } from '@ngrx/store';
+import * as ShoppingListActions from './shopping-list.actions';
 
 import { Ingredient } from '../../shared/ingredient.model';
-
-export const ADD_INGREDIENT = 'ADD_INGREDIENT'
 
 const initialState = {
   ingredients: [
@@ -11,13 +9,15 @@ const initialState = {
   ]
 }
 
-export function shoppingListReducer(state, action: Action) {
+export function shoppingListReducer(state, action: ShoppingListActions.ShoppingListActions) {
   switch (action.type) {
-    case ADD_INGREDIENT:
+    case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients, action]
-      }
+        ingredients: [...state.ingredients, action.payload]
+      };
+    default:
+      return state;
   }
 
   return state;

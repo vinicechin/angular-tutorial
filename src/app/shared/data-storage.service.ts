d55b9468c8cddac4ea321b64b395eebd5cae1ade
Vinicius Cechin <vinicechin@gmail.com>
2018-06-12 10:45:13 -0300

    added get method

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -9,5 +10,15 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put('https://ng-recipe-book-e06c0.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get('https://ng-recipe-book-e06c0.firebaseio.com/recipes.json')
+      .subscribe(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
